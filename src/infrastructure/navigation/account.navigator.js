fbc2f91fe8abccf1a8f540fc16fbf9164e1a7d1f
zhengx55 <williamzhxin@gmail.com>
2023-07-14 17:55:46 -0400

    opt

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -8,7 +8,7 @@ import { RegisterScreen } from '../../features/account/screens/register.screen';
 const Stack = createStackNavigator();
 
 export const AccountNavigator = () => (
-  <Stack.Navigator headerMode="none">
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Main" component={AccountScreen} />
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Register" component={RegisterScreen} />
